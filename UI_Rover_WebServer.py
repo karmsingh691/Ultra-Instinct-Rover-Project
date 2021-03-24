3b4009c785abb572b7b51041e17d76b52319304d
Karmdeep Singh <55263663+karmsingh691@users.noreply.github.com>
2021-03-23 18:56:32 -0700

    Update UI_Rover_WebServer.py
    Made some comments and introduction to the program

--- UI_Rover_WebServer.py
+++ UI_Rover_WebServer.py
@@ -1,11 +1,24 @@
+# Karmdeep Singh
+# Ultra Instinct Prototype Rover Project
+# HTML based webserver to control the rover
+
+# Summary: The goal for this program is to design a webserver with the raspberry pi
+#          to control the rover's movements. 
+
+# The code below is a modified version of a program that I had found online that 
+# used the webserver to control the GPIO pins on the Pi and turn on/off an LED
+
+# Source Code --> https://www.e-tinkers.com/2018/04/how-to-control-raspberry-pi-gpio-via-http-web-server/ 
+
 import RPi.GPIO as GPIO
 import os
 import Obstacle_Advoidance 
 from time import sleep
+# Python HTTP Server Library
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from roboclaw_3 import Roboclaw
 host_name = ''  # Change this to your Raspberry Pi IP address
-host_port = 
+host_port = ''  # Can choose from 1024 to 65353
 
 GPIO.setmode(GPIO.BOARD)
 
@@ -31,6 +44,8 @@ class MyServer(BaseHTTPRequestHandler):
         """ do_GET() can be tested using curl command
             'curl http://server-ip-address:port'
         """
+        # HTML Code for the Webserver
+        
         html = '''
            <html>
            <body style="width:960px; margin: 50px auto;">
@@ -50,14 +65,23 @@ class MyServer(BaseHTTPRequestHandler):
            </body>
            </html>
         '''
+        # Assign CPU Temperature to be saved in a variable
+        
         temp = os.popen("vcgencmd measure_temp").read()
         self.do_HEAD()
         status = ''
         
+        # The following conditional statements check if the URL changes. Certain URL's string along with the 
+        # ip adddress and port number are programmed to do certain actions. These actions are connected
+        # with the HTML code above
+      
+        # Initial Start: Setup Pins
         if self.path=='/':
+            
             GPIO.setmode(GPIO.BOARD)
             GPIO.setup(37, GPIO.OUT)
-        elif self.path=='/forward':
+            
+        elif self.path=='/forward': # if URL is "IP address:port/forward" --> Move Rover forward
 
             roboclaw.ForwardM1(address,80)
             roboclaw.ForwardM2(address, 80)
@@ -82,17 +106,16 @@ class MyServer(BaseHTTPRequestHandler):
             roboclaw.ForwardM1(address,0)
             roboclaw.ForwardM2(address, 0)
        
-        elif self.path=='/start':
+        elif self.path=='/start': # Start Obstacle Avoidance Program
             Obstacle_Advoidance.obstacle_avoidance()
 
             
-        self.wfile.write(html.format(temp[5:], status).encode("utf-8"))
+        self.wfile.write(html.format(temp[5:], status).encode("utf-8")) # Format the temperature reading
 
 
 if __name__ == '__main__':
     http_server = HTTPServer((host_name, host_port), MyServer)
     print("Server Starts - %s:%s" % (host_name, host_port))
-
     try:
         http_server.serve_forever()
     except KeyboardInterrupt:
